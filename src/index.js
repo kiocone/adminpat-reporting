ab2d85736667dd8906207ec0d00b78f395458c9f
r_cruz <r_cruz@farmacity.com.ar>
2024-03-21 20:57:36 -0500

    feat(fileSystem): add validation to create reports folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 require("dotenv").config()
-const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
 const fileUpload = require("express-fileupload");
-
+const fs = require('fs')
 const port = process.env.port;
 
 const app = express();
-
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
@@ -15,6 +13,14 @@ app.use(express.json());
 app.use(fileUpload({
   limits: { fileSize: 10 * 1024 * 1024 },
 }));
+
+fs.mkdir(process.cwd() + '/reports', (err) => {
+  if (err?.code == 'EEXIST') {
+    console.log('reports folder already exist, PASS!')
+  } else {
+    console.log('Created directory for reports on /reports')
+  }
+})
 
 app.use(require('./routes'));
 
