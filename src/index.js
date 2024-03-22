5d54f0678296db6ba4eeb71f4d7caa77ca5a9b91
r_cruz <r_cruz@farmacity.com.ar>
2024-03-21 20:43:58 -0500

    feat(uploadfile): endpoint to upload files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
+const fileUpload = require("express-fileupload");
 
 const port = process.env.port;
 
@@ -11,6 +12,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(fileUpload({
+  limits: { fileSize: 10 * 1024 * 1024 },
+}));
 
 app.use(require('./routes'));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,24 @@ router.get('/auth', (req, res) => {
     res.status(200).json({response: "Auth EP"})
 });
 
+router.post('/upload', (req, res) => {
+    let incomingFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "incomingFile") is used to retrieve the uploaded file
+    incomingFile = req.files.exampleFile;
+    uploadPath = process.cwd() + '/reports/' + incomingFile.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    incomingFile.mv(uploadPath, (err) => {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
 module.exports = router;
